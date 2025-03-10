8fb46d87d20e54f685d64a84c5558a74085fcb8f
Nour Besbes <nbesbes2@gmail.com>
2025-03-10 01:11:41 +0100

    Adding React app files to my-static-website

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MyFirstComponent from './MyFirstComponent';  
+
+function App() {
+  return (
+    <div>
+      <MyFirstComponent /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MyFirstComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function getTitle(title) {
+  return title;
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Hello {getTitle("React")}</h1>
+      <label htmlFor="search">Search: </label>
+      <input type="text" id="search" />
+    </div>
+  );
+}
+
+export default App;
