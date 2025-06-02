737afd005de76c366382f0d2bedabfe203df4596
Nour Besbes <nbesbes2@gmail.com>
2025-06-02 22:02:45 +0100

    W4 React Multiple Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import MyFirstComponent from './MyFirstComponent';  
+
+const students = [
+  { id: 1, name: "Nour", age: 22 },
+  { id: 2, name: "Ali", age: 21 },
+  { id: 3, name: "Leila", age: 23 }
+];
 
 function App() {
   return (
     <div>
-      <MyFirstComponent /> 
+      <h1>Student List</h1>
+      <ul>
+        {students.map((student) => (
+          <li key={student.id}>
+            {student.name} - Age: {student.age}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
+
 
 export default App;
