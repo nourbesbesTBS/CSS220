fa1ea14224847e0f287c1b05397e16070c9467d1
Nour Besbes <nbesbes2@gmail.com>
2025-06-28 13:10:13 +0100

    W6 Adding React props

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const students = [
-  { id: 1, name: "Nour", age: 22 },
-  { id: 2, name: "Ali", age: 21 },
-  { id: 3, name: "Leila", age: 23 }
-];
+const Item = ({ item }) => {
+  return (
+    <li>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>{' '}
+      <span>{item.author}</span>{' '}
+      <span>{item.num_comments}</span>{' '}
+      <span>{item.points}</span>
+    </li>
+  );
+};
 
-function App() {
+const List = ({ list }) => {
+  return (
+    <ul>
+      {list.map((item) => (
+        <Item key={item.objectID} item={item} />
+      ))}
+    </ul>
+  );
+};
+
+const Search = ({ searchTerm, onSearch }) => {
   return (
     <div>
-      <h1>Student List</h1>
-      <ul>
-        {students.map((student) => (
-          <li key={student.id}>
-            {student.name} - Age: {student.age}
-          </li>
-        ))}
-      </ul>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" value={searchTerm} onChange={onSearch} />
     </div>
   );
-}
+};
 
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (event) => {
+    console.log(event.target.value);
+    setSearchTerm(event.target.value);
+  };
+
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+      <Search searchTerm={searchTerm} onSearch={handleSearch} />
+      <hr />
+      <List list={stories} />
+    </div>
+  );
+};
 
 export default App;
